fd265900958e38bcda623473b20957993a37f8b8
Lainlnya <kimyelim100@gmail.com>
2023-02-11 23:22:24 +0900

    solving programmers problem(Lv.1)

--- 나누어떨어지는숫자배열_1.js
+++ 나누어떨어지는숫자배열_1.js
@@ -8,7 +8,3 @@ function solution(arr, divisor) {
 
   return answer.sort((a, b) => a - b);
 }
-
-console.log(solution([5, 9, 7, 10], 5));
-console.log(solution([2, 36, 1, 3], 1));
-console.log(solution([3, 2, 6], 10));
