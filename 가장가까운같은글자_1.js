5f9c73f50e893da9a54e3ee6ca12b1580560f851
Lainlnya <kimyelim100@gmail.com>
2023-02-23 23:17:55 +0900

    solving programmers problems(Lv.1)

---
+++ 가장가까운같은글자_1.js
@@ -0,0 +1,12 @@
+function solution(s) {
+  let answer = [];
+  s.split('').map((v, i) => {
+    if (i === s.indexOf(v)) {
+      answer.push(-1);
+    } else answer.push(i - s.lastIndexOf(v, i - 1));
+  });
+  return answer;
+}
+
+console.log(solution('banana'));
+console.log(solution('foobar'));
